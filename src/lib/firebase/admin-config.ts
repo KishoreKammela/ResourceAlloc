5dccdce69ce5b44032074da00db285b64c11e029
kishore kammela <kishorekammela@gmail.com>
2025-07-21 10:48:31 +0000

    Getting this error
    500: INTERNAL_SERVER_ERROR
    Code: MIDDLEWARE_INVOCATI

--- src/lib/firebase/admin-config.ts
+++ src/lib/firebase/admin-config.ts
@@ -1,18 +1,26 @@
 import * as admin from 'firebase-admin';
 
 export async function initAdmin() {
-  const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
-
-  if (!serviceAccount) {
-    throw new Error(
-      'FIREBASE_SERVICE_ACCOUNT_KEY environment variable is not set.'
-    );
-  }
-
   if (!admin.apps.length) {
     try {
+      const serviceAccount = {
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      };
+
+      if (
+        !serviceAccount.projectId ||
+        !serviceAccount.clientEmail ||
+        !serviceAccount.privateKey
+      ) {
+        throw new Error(
+          'Firebase admin environment variables are not fully set.'
+        );
+      }
+
       admin.initializeApp({
-        credential: admin.credential.cert(JSON.parse(serviceAccount)),
+        credential: admin.credential.cert(serviceAccount),
       });
     } catch (error: any) {
       console.error('Firebase admin initialization error', error.stack);
